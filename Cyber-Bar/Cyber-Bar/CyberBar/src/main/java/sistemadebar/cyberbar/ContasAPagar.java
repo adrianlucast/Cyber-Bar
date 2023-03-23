5aa0159f49bb20696906bc3a64980926890d3067
Lucas Pessoli <pessolilucas@gmail.com>
2023-03-22 22:33:38 -0300

    Update ContasAPagar.java
    Arrumar o switch

--- Cyber-Bar/Cyber-Bar/CyberBar/src/main/java/sistemadebar/cyberbar/ContasAPagar.java
+++ Cyber-Bar/Cyber-Bar/CyberBar/src/main/java/sistemadebar/cyberbar/ContasAPagar.java
@@ -4,6 +4,7 @@
  */
 package sistemadebar.cyberbar;
 import java.util.Date;
+import java.util.Scanner;
 
 
 /**
@@ -24,6 +25,16 @@ public class ContasAPagar {
     public double totalAPagar;
     public int parcela;
     
+    Scanner scn = new Scanner(System.in);
+    
+    public void setValorConta(double valorConta){
+        this.valorConta = valorConta;
+    }
+    
+    public double getValorConta(){
+        return this.valorConta;
+    }
+    
     public void setSaldo(double saldo){
         this.saldo = saldo;
     }
@@ -40,16 +51,36 @@ public class ContasAPagar {
         return this.idConta;
     }
     
-    public ContasAPagar(int idConta, double valorConta, String nomeConta){
-        this.idConta = idConta;
-        this.valorConta = valorConta;
-        this.nomeConta = nomeConta;
+    public void AdicionaSaldo(){
+        System.out.println("Digite a quantia que você quer adicionar de saldo: ");
+        int saldoadd = scn.nextInt();
+        System.out.println("Qual forma de pagamento você escolherá\n 1 - PIX\n 2 - Cartão de crédito/débito \n 3 - Boleto bancário\n 4 - Recarga");
+        char metodoEscolha = scn.next().charAt(0);
+        switch (metodoEscolha){
+            case '1':
+                System.out.println("Você escolheu:  PIX!");
+                String escolhaPix = "PIX";
+                break;
+            case '2':
+                System.out.println("Você escolheu: Cartão de crédito/débito!");
+                String escolhaCartao = "Cartão de crédito/débito";
+                break;
+            case '3':
+                System.out.println("Você escolheu: Boleto Bancário!");
+                String escolhaBoleto = "Boleto bancário";
+                break;
+            case '4':
+                System.out.println("Você escolheu: Recarga!");
+                String escolhaRecarga = "Recarga";
+                break;
+        }
+        System.out.println("Você escolheu: ");
+        
     }
     
-    public void Pagar(String nomeConta, int idConta, double valorConta){
+    public void Pagar(int idConta, String nomeConta, double valorConta){
         this.saldo = (getSaldo() - valorConta);
-        System.out.println("Novo saldo: " + saldo);
-        System.out.println("");
+        System.out.println("Conta:"+nomeConta+" foi paga com sucesso! "+"\nNovo saldo: " + saldo);
     }
     
 }
